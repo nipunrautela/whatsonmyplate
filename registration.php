4f21ab7ab26082ae82afca57e35dd634677545c6
Nipun Rautela <nipunrautela261@gmail.com>
2022-04-30 15:45:10 +0530

    first upload to heroku

--- registration.php
+++ registration.php
@@ -1,25 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style/base.css">
-    <link rel="stylesheet" href="./style/form.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Register</title>
-
-    <script>
-      function hidepassword() {
-        var pwd = document.getElementById('password')
-        pwd.type = "password";
-      }
-      
-      function showpassword() {
-        var pwd = document.getElementById('password')
-        pwd.type = "text";
-      }
-    </script>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./style/base.css">
+  <link rel="stylesheet" href="./style/form.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>Register</title>
+  <script src="./script/base.js"></script>
 </head>
 <body>
     <?php include './navbar.html'; ?>
@@ -35,7 +24,7 @@
           </div>
         </div>
         <div class="form-spacer"></div>
-        <form action="signin.php" method="post">
+        <form action="registration.php" method="post">
           <div class="form-field">
             <input type="text" class="form-field-input" placeholder=" " id="name" />
             <label for="username" class="form-field-label">Full Name</label>
@@ -76,7 +65,7 @@
             </label>
           </div>
           <div>
-            <button class="form-submit-button">Sign In</button>
+            <button class="form-submit-button">Register</button>
           </div>
           <div class="form-links">
             Already have an account?
@@ -92,3 +81,11 @@
     
 </body>
 </html>
+<?php
+  if ($_SERVER["REQUEST_METHOD"] != "post")
+    die;
+  $conn = mysqli_connect("localhost", "cse3002", "Test@0987");
+  if (!$conn)
+    print mysqli_error($conn);
+  print $conn
+?>
---
+++ script/base.js
@@ -0,0 +1,9 @@
+function hidepassword() {
+  var pwd = document.getElementById("password");
+  pwd.type = "password";
+}
+
+function showpassword() {
+  var pwd = document.getElementById("password");
+  pwd.type = "text";
+}
