63c71d071a4995a4c42c4ebd6fba290a72eec880
Nipun Rautela <nipunrautela261@gmail.com>
2022-04-30 11:21:24 +0530

    setup new pages and small head change in all pages

--- profile.php
+++ profile.php
@@ -4,32 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style/base.css">
-    <link rel="stylesheet" href="./style/profile.css">
-    <title>Profile</title>
+    <link rel="stylesheet" href="./style/base.css" />
+    <link rel="stylesheet" href="./style/home.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <title>My Profile</title>
 </head>
 <body>
-    <div class="navbar">
-      <div class="navbar-heading"><a href=".">WOMP</a></div>
-      <ul>
-        <li>
-          <a href=".">Home</a>
-        </li>
-        <li>
-          <a href="#">Food Database</a>
-        </li>
-        <li>
-          <a href="#">Popular Diets</a>
-        </li>
-        <li>
-          <a href="#">Consult</a>
-        </li>
-        <li>
-          <a href="#">My Profile</a>
-        </li>
-      </ul>
-    </div>
-
+    <?php include './navbar.html'; ?>
     
 </body>
 </html>
